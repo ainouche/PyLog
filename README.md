fbeacaf918e9a1b03a0ff5f7125e58df6abaf80e
Hannah Twigg-Smith <htwiggsmith@gmail.com>
2017-04-15 11:53:39 -0400

    Beautify files

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # PyLog
-Pylog is Prolog-ish interpreter written in Python 3. It originated as a final project for my Programming Languages class, but I wanted to take it further because this stuff is neat.
+
+Pylog is Prolog-ish interpreter written in Python 3\. It originated as a final project for my Programming Languages class, but I wanted to take it further because this stuff is neat.
 
 ## Grammar
+
 ```
 <expr> ::= <integer>
             true
@@ -13,10 +15,10 @@ Pylog is Prolog-ish interpreter written in Python 3. It originated as a final pr
 <rule> ::= name(constant, ... ) :- name(constant, ...), ... .
 ```
 
-
 ## Using PyLog
 
 You can start the repl by typing `python pylog.py`:
+
 ```
 $ python pylog.py
 
@@ -28,6 +30,7 @@ pylog>
 ### Facts
 
 Facts are the building blocks of PyLog. Defining a fact adds the fact to the environment. You define a fact by following it with a period:
+
 ```
 pylog> happy(hannah).
 Fact happy defined
@@ -41,6 +44,7 @@ Fact loves defined
 ```
 
 However, once you define a fact/relation it must always be given the same number of constants:
+
 ```
 pylog> loves(hannah).
 Error: Fact loves takes 2 argument(s), 1 given.
@@ -63,20 +67,24 @@ pylog> loves(mary,john).
 ```
 
 Now we can find all of the people who love Sally:
+
 ```
 pylog> loves(X,sally)?
 loves('mary', 'sally').
 loves('john', 'sally').
 ```
+
 Notice that facts are positional.
 
 ### Rules
 
 Rules are the most useful part of Prolog/Datalog. You can write some pretty neat stuff with them; one of my favorite examples is the [sudoku solver](https://programmablelife.blogspot.com/2012/07/prolog-sudoku-solver-explained.html). To define a rule in PyLog, you use the `:-` operator:
+
 ```
 pylog> notsad(X) :- happy(X).
 Rule notsad defined
 ```
+
 This rule simply says that someone being not sad is the same as them being happy. Now, we can check for facts that satisfy this rule's conditions:
 
 ```
@@ -87,12 +95,14 @@ yes
 Currently, rule queries can't handle variables. That will change as soon as I get around to it.
 
 Rules can take multiple facts, too:
+
 ```
 pylog> foobar(X) :- foo(X), bar(X).
 Rule foobar defined
 ```
 
 Facts have to satisfy all of the rule's conditions in order to satisfy the rule:
+
 ```
 pylog> bar(bob).
 Fact bar defined
@@ -104,7 +114,9 @@ pylog> bar(joe).
 pylog> foobar(joe)?
 no
 ```
+
 ### Env
+
 The `#env` command prints out everything that is currently in the environment. Here we see how everything is stored:
 
 ```
@@ -113,15 +125,16 @@ Relations:
 {'loves': [['mary', 'sally'], ['bob', 'sally'], ['john', 'sally'], ['mary', 'john']], 'foo': [['bob']],
 'bar': [['bob'], ['joe']], 'happy': [['hannah']]}
 Rules:
-{'foobar': {'body': [('foo', (['X'], {})), ('bar', (['X'], {}))], 'vars': ['X']}, 'notsad': {'body': 
+{'foobar': {'body': [('foo', (['X'], {})), ('bar', (['X'], {}))], 'vars': ['X']}, 'notsad': {'body':
 [('happy', (['X'], {}))], 'vars': ['X']}}
 ```
 
 ### Quit
+
 You can quit PyLog by typing `#quit`. See you later!
 
-
 ## Future Work
+
 - Implement recursive rules
 - Allow rules to handle intermediate variables. Ex: `grandparent(X,Z) :- parent(X,Y), parent(Y,Z).` Shouldn't be too hard.
 - Implement and, or, and not in rules.
--- pylog.py
+++ pylog.py
@@ -14,6 +14,7 @@
 # Prolog-style interpreter
 #
 
+
 def check_for_vars(body):
     cont_vars = False
     var_positions = []
@@ -35,9 +36,9 @@ def unify_relation(relations, rules, name, body):
     cont_vars, var_positions = check_for_vars(body)
     matches = []
 
-    #attempt to unify terms
+    # attempt to unify terms
     if cont_vars:
-        #if the query body contains variables, find matching facts
+        # if the query body contains variables, find matching facts
 
         for relation in relations[name]:
             match = True
@@ -53,7 +54,7 @@ def unify_relation(relations, rules, name, body):
 
         return (name, matches)
 
-    #return yes or no if there are only constants
+    # return yes or no if there are only constants
     if body in relations[name]:
         return "yes"
     return "no"
@@ -61,19 +62,19 @@ def unify_relation(relations, rules, name, body):
 
 def unify_rule(relations, rules, name, body):
 
-    local_env = dict(zip(rules[name]["vars"],body))
+    local_env = dict(zip(rules[name]["vars"], body))
 
     for rel in rules[name]["body"]:
         rel = list(rel)
         rel[1] = rel[1].asList()
 
-        for i,v in enumerate(rel[1]):
+        for i, v in enumerate(rel[1]):
             if v in local_env:
                 rel[1][i] = local_env[v]
             elif v[0].isupper:
                 print("INTERMEDIATE VARIABLE")
 
-        ans = top_unify(relations,rules,rel[0],rel[1])
+        ans = top_unify(relations, rules, rel[0], rel[1])
 
         if ans == "no":
             return ans
@@ -96,17 +97,18 @@ def top_unify(relations, rules, name, body):
         return "Error: {} not found in environment.".format(name)
 
 
-
 ############################################################
 ##
-## Pylog parser
+# Pylog parser
 ##
 # cf http://pyparsing.wikispaces.com/
 
-from pyparsing import Word, ZeroOrMore, Keyword, alphas, NoMatch, Group, Suppress
+
+from pyparsing import (Group, Keyword, NoMatch, Suppress, Word, ZeroOrMore,
+                       alphas)
 
 
-def initial_env ():
+def initial_env():
     # environment is defined as a dictionary
     relations = {}
     rules = {}
@@ -114,7 +116,7 @@ def initial_env ():
     return relations, rules
 
 
-def parse_imp (input):
+def parse_imp(input):
     # Grammar:
     #
     # <expr> ::= <integer>
@@ -127,41 +129,43 @@ def parse_imp (input):
     # <rule> ::= name(constant, ... ) :- name(constant, ...), ... .
     #
 
-    idChars = alphas+"_+*!=<>"
+    idChars = alphas + "_+*!=<>"
 
-    pNAME = Word(idChars,idChars+"0123456789")
+    pNAME = Word(idChars, idChars + "0123456789")
 
-    pRELATION = pNAME + "(" + Group(pNAME + ZeroOrMore(Suppress(",") + pNAME)) + ")"
+    pRELATION = pNAME + \
+        "(" + Group(pNAME + ZeroOrMore(Suppress(",") + pNAME)) + ")"
     pRELATION.setParseAction(lambda result: (result[0], result[2]))
 
     pDECL_RELATION = pRELATION + "."
     pDECL_RELATION.setParseAction(lambda result: result[0])
 
-    pRULE = pRELATION + ":-" + Group(pRELATION + ZeroOrMore(Suppress(",") + pRELATION)) + "."
+    pRULE = pRELATION + ":-" + \
+        Group(pRELATION + ZeroOrMore(Suppress(",") + pRELATION)) + "."
     pRULE.setParseAction(lambda result: (result[0], result[2]))
 
-    pFACT = ( pDECL_RELATION ^ NoMatch() )
+    pFACT = (pDECL_RELATION ^ NoMatch())
 
     pQUERY = pRELATION + "?"
     pQUERY.setParseAction(lambda result: (result[0][0], result[0][1]))
 
     pTOP_QUERY = pQUERY.copy()
-    pTOP_QUERY.setParseAction(lambda result: {"result":"query",
-                                             "stmt":result[0]})
+    pTOP_QUERY.setParseAction(lambda result: {"result": "query",
+                                              "stmt": result[0]})
 
     pTOP_RULE = pRULE.copy()
-    pTOP_RULE.setParseAction(lambda result: {"result":"rule",
-                                             "rule":result})
+    pTOP_RULE.setParseAction(lambda result: {"result": "rule",
+                                             "rule": result})
 
     pTOP_FACT = pFACT.copy()
-    pTOP_FACT.setParseAction(lambda result: {"result":"fact",
-                                             "decl":result[0]})
+    pTOP_FACT.setParseAction(lambda result: {"result": "fact",
+                                             "decl": result[0]})
 
     pQUIT = Keyword("#quit")
-    pQUIT.setParseAction(lambda result: {"result":"quit"})
+    pQUIT.setParseAction(lambda result: {"result": "quit"})
 
     pENV = Keyword("#env")
-    pENV.setParseAction(lambda result: {"result":"env"})
+    pENV.setParseAction(lambda result: {"result": "env"})
 
     pTOP = (pQUIT ^ pENV ^ pTOP_RULE ^ pTOP_FACT ^ pTOP_QUERY)
 
@@ -169,7 +173,7 @@ def parse_imp (input):
     return result    # the first element of the result is the expression
 
 
-def shell ():
+def shell():
     # A simple shell
     # Repeatedly read a line of input, parse it, and evaluate the result
 
@@ -191,7 +195,7 @@ def shell ():
                     print(v)
                 else:
                     for i in v[1]:
-                        print("{}{}.".format(v[0],tuple(i)))
+                        print("{}{}.".format(v[0], tuple(i)))
 
             elif result["result"] == "quit":
                 return
@@ -213,8 +217,8 @@ def shell ():
                 # else:
                 # Maybe later
 
-                rules[res[0][0]] = {"vars":res[0][1].asList(),
-                                    "body":res[1]}
+                rules[res[0][0]] = {"vars": res[0][1].asList(),
+                                    "body": res[1]}
 
                 print("Rule {} defined".format(res[0][0]))
 
@@ -231,16 +235,17 @@ def shell ():
                     print("Error: Can't define a fact with a variable!")
                 elif name in relations:
                     if len(constants) != len(relations[name][0]):
-                        print("Error: Fact {} takes {} argument(s), {} given.".format(name,len(relations[name][0]),len(constants)))
+                        print("Error: Fact {} takes {} argument(s), {} given.".format(
+                            name, len(relations[name][0]), len(constants)))
                     else:
                         relations[name].append(constants.asList())
                 else:
                     relations[name] = [constants.asList()]
                     print("Fact {} defined".format(name))
 
-
         except Exception as e:
             print("Exception: {}".format(e))
+
 
 if __name__ == '__main__':
     shell()
